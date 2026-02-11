0e479ad51c8f8ca416cc263cb9715c1d7a06f7d5
Jacob Wisniewski <jacobawisniewski@gmail.com>
2026-02-10 22:43:19 -0500

    add trip list

--- app_admin/src/app/app.ts
+++ app_admin/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { TripListing } from './trip-listing/trip-listing';
+  
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TripListing],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('travlr-admin');
+  protected readonly title = signal('Travlr Getaways Admin!');
 }
