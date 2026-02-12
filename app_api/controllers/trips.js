502aaf3385e91ac53309b4369ff0c5fdf1e2c58e
Jacob Wisniewski <jacobawisniewski@gmail.com>
2026-02-11 21:59:18 -0500

    add trip functionality

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -28,7 +28,30 @@ const tripsFindByCode = async (req, res) => {
     }
 };
 
+// Post: /trips - adds a new trip to the database
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsAddTrip = async (req, res) => {
+    const newTrip = new Trip({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    });
+
+    const q = await newTrip.save().catch((err) => {
+        return res.status(400).json(err);
+    });
+
+    return res.status(201).json(q);
+};
+
 module.exports = {
     tripsList,
     tripsFindByCode,
+    tripsAddTrip
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const tripsController = require('../controllers/trips');
 
-// Route to get the list of all trips
-router.route('/trips').get(tripsController.tripsList);
+// Define the route for /trips with GET and POST methods
+router
+    .route('/trips')
+    .get(tripsController.tripsList) // GET Method routes tripsList
+    .post(tripsController.tripsAddTrip); // POST Method Add a new trip
 
 // GET Method routes tripsFindByCode - requries parameter
 router.route('/trips/:tripCode').get(tripsController.tripsFindByCode);
