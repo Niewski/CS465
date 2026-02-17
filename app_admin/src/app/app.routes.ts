ce1ed3b6c7df91ee22a2f2e32e5988d4e39ea097
Jacob Wisniewski <jacobawisniewski@gmail.com>
2026-02-16 23:42:08 -0500

    added auth and login to angular app

--- app_admin/src/app/app.routes.ts
+++ app_admin/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { AddTrip } from './add-trip/add-trip';
 import { EditTrip } from './edit-trip/edit-trip';
+import { Login } from './login/login';
 import { TripListing } from './trip-listing/trip-listing';
 
 export const routes: Routes = [
     { path: '', component: TripListing, pathMatch: 'full' },
     { path: 'add-trip', component: AddTrip },
-    { path: 'edit-trip', component: EditTrip }
+    { path: 'edit-trip', component: EditTrip },
+    { path: 'login', component: Login }
 ];
