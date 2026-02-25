1027beff86e8564d602843a984db0d37deab8128
Jacob Wisniewski <jacobawisniewski@gmail.com>
2026-02-25 14:11:46 -0500

    added routes and handlers

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,8 +1,21 @@
-/* GET Homepage */
 const index = (req, res) => {
   res.render('index', { title: 'Travlr Getaways' });
 }
 
-module.exports = {
-  index,
-};
+const reservations = (req, res) => {
+  if (!res.locals.user) {
+    return res.redirect('http://localhost:4200/login');
+  }
+  res.render('reservations', { title: 'Reservations - Travlr Getaways' });
+}
+
+const checkout = (req, res) => {
+  res.render('checkout', { title: 'Checkout - Travlr Getaways' });
+}
+
+const logout = (req, res) => {
+  res.clearCookie('travlr-token', { path: '/' });
+  res.redirect('/');
+}
+
+module.exports = { index, reservations, checkout, logout };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 const ctrlMain = require('../controllers/main');
 
-/* GET home page. */
 router.get('/', ctrlMain.index);
+router.get('/reservations', ctrlMain.reservations);
+router.get('/checkout', ctrlMain.checkout);
+router.get('/logout', ctrlMain.logout);
 
 module.exports = router;
