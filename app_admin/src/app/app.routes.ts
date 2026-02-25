473bb0e0050e9b428a8e5fb812361a95c67c1d1b
Jacob Wisniewski <jacobawisniewski@gmail.com>
2026-02-24 22:48:09 -0500

    added signup route

--- app_admin/src/app/app.routes.ts
+++ app_admin/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { AddTrip } from './add-trip/add-trip';
 import { EditTrip } from './edit-trip/edit-trip';
 import { Login } from './login/login';
+import { Signup } from './signup/signup';
 import { TripListing } from './trip-listing/trip-listing';
 
 export const routes: Routes = [
     { path: '', component: TripListing, pathMatch: 'full' },
     { path: 'add-trip', component: AddTrip },
     { path: 'edit-trip', component: EditTrip },
-    { path: 'login', component: Login }
+    { path: 'login', component: Login },
+    { path: 'signup', component: Signup }
 ];
